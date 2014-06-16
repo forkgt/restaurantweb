fa7d96d3f555bd24afaa062c64b2ccd258be7a09
feng <wanfenghuaian@gmail.com>
2014-06-16 13:50:08 -0500

    Use erb in yml config files

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,9 @@
 require File.expand_path('../application', __FILE__)
 
 # Load the app_config.yml file
-APP_CONFIG = YAML.load_file(Rails.root.join('config', 'app_config.yml'))[Rails.env]
+#APP_CONFIG = YAML.load_file(Rails.root.join('config', 'app_config.yml'))[Rails.env]
+# Load the app_config.yml file and read erb
+APP_CONFIG = YAML.load(ERB.new(File.read(Rails.root.join('config', 'app_config.yml'))).result)[Rails.env]
 
 # Initialize the Rails application.
 Ibm::Application.initialize!
